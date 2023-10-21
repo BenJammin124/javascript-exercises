a5bb05895b9b8464f714374c73dd76a39357b1a4
BenJammin <bmonty124@outlook.com>
2023-10-21 18:07:12 -0400

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+let rounded = dec => {
+  dec = Math.round(dec * 10) / 10
+  return dec;
+}
+
+
+const convertToCelsius = function (x) {
+
+  x = (x - 32) * 5 / 9;
+  x = rounded(x)
+  return x;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function (y) {
+  y = (y * 9 / 5 + 32)
+  y = rounded(y);
+  return y
 };
 
 // Do not edit below this line
