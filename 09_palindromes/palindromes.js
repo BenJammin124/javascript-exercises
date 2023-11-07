8016ce9a0da2afa22b1b3eca1c7897ef34a95235
BenJammin <bmonty124@outlook.com>
2023-11-07 18:18:18 -0500

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (pali) {
+    let reversedPali = "";
+    let str = pali.replace(/[^\w\s\']|_/g, "")
+        .replace(/\s+/g, "")
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedPali += str[i];
+    }
+    if (str.toLowerCase() == reversedPali.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    };
+}
+console.log()
 // Do not edit below this line
 module.exports = palindromes;
