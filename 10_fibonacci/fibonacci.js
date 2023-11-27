43f62b98e5bb531b2df3adf837d852e1473116ed
BenJammin <bmonty124@outlook.com>
2023-11-27 18:37:22 -0500

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    if (n < 0) {
+        return "OOPS"
+    }
+    for (let i = 1; i < n; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+
+    } return num2;
 
 };
 
